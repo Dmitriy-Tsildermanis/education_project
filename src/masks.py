ccb23cf82d5b175f941a3caef525d350bb7e766b
Dmitriy Tsildermanis <dmitry.tsildermanis@ya.ru>
2025-11-29 22:23:24 +0400

    Исправлен некорректный вывод маски

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_number: int) -> str:
     """Функция принимает на вход номер карты и возвращает маску по образцу ХХХХ ХХ** **** ХХХХ, где Х - цифра номера"""
     digit_list = [digit for digit in str(card_number)]
-    mask_card_number = f"{''.join(digit_list[0:4])} {''.join(digit_list[4:6])} ** **** {''.join(digit_list[-4:])}"
+    mask_card_number = f"{''.join(digit_list[0:4])} {''.join(digit_list[4:6])}** **** {''.join(digit_list[-4:])}"
     return mask_card_number
 
 
