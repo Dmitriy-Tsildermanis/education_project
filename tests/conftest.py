7fbcb37e49ac7d231d6bcf6b9a2ce5fe525946f3
Dmitriy Tsildermanis <dmitry.tsildermanis@ya.ru>
2025-12-09 01:10:13 +0400

    create tests for module processing.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,3 +24,47 @@ def valid_account_mask():
 @pytest.fixture
 def negative_number_int():
     return -1234567890123456
+
+
+@pytest.fixture
+def state_executed():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+@pytest.fixture
+def state_canceled():
+    return [{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def valid_dict_of_processing():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+
+@pytest.fixture
+def valid_dict_of_processing_sorted_decrease():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+
+
+
+@pytest.fixture
+def valid_dict_of_processing_sorted_increase():
+    return [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}]
+
+@pytest.fixture
+def valid_dict_of_processing_same_date():
+    return [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
