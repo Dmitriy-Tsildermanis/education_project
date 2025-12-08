a780e16c6e0825d31c793b49fb2769a1aaa93fac
Dmitriy Tsildermanis <dmitry.tsildermanis@ya.ru>
2025-12-08 14:23:56 +0400

    add constants.py and refactor test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,17 +1,6 @@
 import pytest
 from src.masks import get_mask_account, get_mask_card_number
-
-INVALID_TYPE_NUMBERS = [12.45,
-                        [123, [123, 123]],
-                        [[1], [2], [3], [4], [4], 3, 5, 6, 7, 8, 9, 3, 4, 5, 6, 7],
-                        {},
-                        (),
-                        '',
-                        '1234 5678 9012 3456',
-                        '1234-5679-0123-4567',
-                        'asdfqwerasdfqwer',
-                        '1234qwer5678qwer',
-                        None]
+from tests.constants import INVALID_TYPE_NUMBERS, INVALID_LENGTHS
 
 
 def test_get_mask_card_number_valid_int(valid_number_int):
@@ -30,11 +19,7 @@ def test_get_mask_card_number_edge_cases(edge_cases, expected_mask):
     assert get_mask_card_number(edge_cases) == expected_mask
 
 
-@pytest.mark.parametrize("invalid_length", [1234,
-                                            '1234',
-                                            0,
-                                            12345678901234567890,
-                                            '12345678901234567890'])
+@pytest.mark.parametrize("invalid_length", INVALID_LENGTHS)
 def test_get_mask_card_number_invalid_length(invalid_length):
     with pytest.raises(ValueError) as exc_info:
         get_mask_card_number(invalid_length)
@@ -84,11 +69,7 @@ def test_get_mask_account_invalid_type(invalid_type):
     assert str(exc_info.value) == "Не верный тип данных"
 
 
-@pytest.mark.parametrize("invalid_length", [1234,
-                                            '1234',
-                                            0,
-                                            12345678901234567890,
-                                            '12345678901234567890'])
+@pytest.mark.parametrize("invalid_length", INVALID_LENGTHS)
 def test_get_mask_account_invalid_length(invalid_length):
     with pytest.raises(ValueError) as exc_info:
         get_mask_account(invalid_length)
